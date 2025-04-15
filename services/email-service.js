4cd051bd3d89094f726c278e70355896304bd737
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2025-04-14 21:33:29 -0500

    ADD emailService

---
+++ services/email-service.js
@@ -0,0 +1,38 @@
+class EmailService {
+
+    async sendWelcomeEmail(email, name) {
+        const emailData = {
+            subject: "¡Bienvenido a MoneyShare!",
+            to: email,
+            dataTemplate: { name },
+            templateName: "registro.html"
+        };
+
+        try {
+            const response = await this.sendEmail(emailData);
+            console.log("Correo enviado:", response);
+        } catch (error) {
+            console.error("Error al enviar el correo:", error);
+        }
+    }
+
+    async sendEmail(emailData) {
+        const response = await fetch("https://send-email-function-ms.azurewebsites.net/api/emailservice", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(emailData)
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        const result = await response.text();
+        return result;
+    }
+
+}
+
+export default EmailService;
--- services/user-service.js
+++ services/user-service.js
@@ -1,10 +1,14 @@
+import EmailService from "./email-service.js";
+
 class UserService {
 
   constructor(userRepository) {
     this.userRepository = userRepository;
+    this.emailService = new EmailService();
   }
 
   async register(info) {
+    await this.emailService.sendWelcomeEmail(info.email, info.name)
     return await this.userRepository.register(info);
   }
 
